0b2863181add1af9d3ad3b8740f66f4b79a6ac4e
GAURAV SHARMA <gsharma1154@gmail.com>
2023-05-22 05:55:05 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/7a/109c18c160f7001d140aee200a934539
@@ -0,0 +1,10 @@
+package in.ineuorn.ai;
+
+public class StreamApi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Stream/src/in/ineuorn/ai/StreamApi.java
+++ Stream/src/in/ineuorn/ai/StreamApi.java
@@ -1,10 +1,31 @@
 package in.ineuorn.ai;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamApi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		//with using list.of,it is immutable in nature .we cannot add the lists .
+List<Integer> list1=List.of(4,56,75,156,865);
+System.out.println(list1);
 
+//2nd method of list making 
+List<Integer> list2=new ArrayList();
+list2.add(50);
+list2.add(30);
+list2.add(20);
+list2.add(10);
+list2.add(0);
+System.out.println(list2);
+
+
+//3rd method of list making 
+
+List<Integer> list3=Arrays.asList(50,34,234,24,24);
+System.out.println(list3);
 	}
 
 }
